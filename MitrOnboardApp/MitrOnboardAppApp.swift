dc724267a1e76312f2c6a189d69062e45ba982e8
vennyvirtuoso <vijendra9753@gmail.com>
2023-02-22 02:58:42 +0530

    Initial Commit

---
+++ MitrOnboardApp/MitrOnboardAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MitrOnboardAppApp.swift
+//  MitrOnboardApp
+//
+//  Created by Vijendra Vaishya on 22/02/23.
+//
+
+import SwiftUI
+
+@main
+struct MitrOnboardAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
